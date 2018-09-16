75a4b825ccb0adf4b9dea8ec262126869033fa74
Charles DeRosier <charlesderosier@hotmail.com>
2018-09-16 11:34:30 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,18 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[1];
-console.log(cardOne);
-console.log(cardTwo);
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length !== 2) {
-}
-else if (cardsInPlay[0] === cardsInPlay[1]) {
+function checkForMatch (){
+if (cardsInPlay[0] === cardsInPlay[2]) {
 	alert("You found a match!");
-}
-else if (cardsInPlay[0] !== cardsInPlay[1]) {
+} else {
 	alert("Sorry, try again.");
 }
-else { 
+};
+function flipCard (cardId) {
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId])
+if (cardsInPlay.length === 2) {
+	checkForMatch();
 }
+};
+flipCard(0);
+flipCard(2);
